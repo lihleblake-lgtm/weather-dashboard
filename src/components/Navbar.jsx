0a44069f0f719137713883ec7e44ac2b509737b4
Thubelihle <lihleblake@gmail.com>
2026-01-04 17:30:13 +0200

    Add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+nav className="flex items-center justify-between p-4 bg-[#FFD6C9]">
+  <button>🏠</button>
+
+  <input
+    type="text"
+    placeholder="Search city"
+    className="px-3 py-1 rounded-lg w-1/2"
+  />
+
+  <button>☰</button>
+</nav>
